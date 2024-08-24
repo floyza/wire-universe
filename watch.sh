7a81ff69bd3dcdbda253786a5cc0c7fd2c0d8a51
Gavin Downard <me@floyza.com>
2024-08-23 17:52:36 -0700

    Properly cleanup when hitting watch.sh with SIGINT

--- watch.sh
+++ watch.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
+trap 'trap - TERM && killall wire-universe_server && kill -- -$$' INT TERM EXIT
+
 # while sleep 2; do fd . client common | entr -dnr wasm-pack build client -t web --no-typescript -d ../assets/pkg; done &
 # while sleep 2; do fd . server common | entr -dnr cargo run; done &
-cargo watch -w common -w client -s "wasm-pack build client -t web --no-typescript -d ../assets/pkg" &
+cargo watch -w common -w client -s "wasm-pack build client --debug -t web --no-typescript -d ../assets/pkg" &
 cargo watch -w common -w server -x run &
 
 wait
